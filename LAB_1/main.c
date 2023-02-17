da5b065fb007af14784719cd244dc2ebcb7e51f9
Alexandr Efimchik <efimchiksasha@gmail.com>
2023-02-18 02:18:36 +0300

    object type color, flag parsing fixed

--- LAB_1/main.c
+++ LAB_1/main.c
@@ -13,6 +13,11 @@
 #define FLAG_DIRECTORIES  1
 #define FLAG_FILES        2
 
+#define PRINT_DIR printf("\033[1;32m");   // green
+#define PRINT_FILE printf("\033[0;37m");  // Blue
+#define PRINT_LINK printf("\033[0;36m");  // Cyan
+#define PRINT_OFF printf("\033[0m");
+
 void directory_browsing( char *introducedDir,  bool* flags, char fl ) // NOLINT(misc-no-recursion)
 {
     DIR *dir = NULL;
@@ -37,16 +42,16 @@ void directory_browsing( char *introducedDir,  bool* flags, char fl ) // NOLINT(
         if(lstat(pathName, &entryInfo) == 0) {
             if(S_ISDIR(entryInfo.st_mode)) {
 			    if(flags[FLAG_DIRECTORIES] || fl == 1) {
-            		printf("\t-d %s\n", pathName);
+                    PRINT_DIR printf("\t-d %s\n", pathName); PRINT_OFF
 			    }
                 directory_browsing(pathName, flags, fl);
             } else if(S_ISREG(entryInfo.st_mode) && (flags[FLAG_FILES] || fl == 1)) {
-            		printf("\t-f %s has %lld bytes\n", pathName, (long long)entryInfo.st_size);
+            		PRINT_FILE printf("\t-f %s has %lld bytes\n", pathName, (long long)entryInfo.st_size); PRINT_OFF
             } else if(S_ISLNK(entryInfo.st_mode)) {
                 char targetName[PATH_MAX + 1];
                 if(readlink(pathName, targetName, PATH_MAX) != -1) {
 				    if(flags[FLAG_SYMLINK] || fl == 1)
-            			printf("\t-l %s -> %s\n", pathName, targetName);
+            			PRINT_LINK printf("\t-l %s -> %s\n", pathName, targetName); PRINT_OFF
                 } else {
 				if(flags[FLAG_SYMLINK] || fl == 1)
             				printf("\t%s -> (invalid symbolic link!)\n", pathName);
@@ -60,8 +65,10 @@ void directory_browsing( char *introducedDir,  bool* flags, char fl ) // NOLINT(
 	closedir(dir);
 }
 
-bool* parse_options (char* args) {
+bool* parse_options (int argc, char* args) {
     bool* arg_flags = (bool*) calloc(3, sizeof (bool));
+    if (argc < 2)
+        return arg_flags;
     arg_flags[FLAG_FILES] = strstr(args, "f") != NULL ? true : false;
     arg_flags[FLAG_DIRECTORIES] = strstr(args, "d") != NULL ? true : false;
     arg_flags[FLAG_SYMLINK] = strstr(args, "l") != NULL ? true : false;
@@ -73,7 +80,7 @@ int main(int argc, char **argv)
 	char option[PATH_MAX];
 	char direct[PATH_MAX];
 	char flag = 0;
-    bool* flags = parse_options(option);
+    bool* flags = parse_options(argc, argv);
     switch (argc) {
         case 1: { // no args
             strcpy(direct,".");
